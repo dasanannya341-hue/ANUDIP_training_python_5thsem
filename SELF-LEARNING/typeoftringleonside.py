678a3752eed01ad3ac94c42119796fd00dbad059
dasanannya341-hue <dasanannya341@gmail.com>
2026-06-03 16:11:15 +0530

    python

--- SELF-LEARNING/typeoftringleonside.py
+++ SELF-LEARNING/typeoftringleonside.py
@@ -14,10 +14,14 @@ if(c <= 0):
 #if the sum of any two sides is greater than the third side then the triangle is valid otherwise it is not valid
 if(a + b > c and a + c > b and b + c > a):
     print("The triangle is valid")
+    #check the type of triangle based on its sides
+    #if all sides are equal then the triangle is an equilateral triangle
     if(a == b == c):
         print("The triangle is an equilateral triangle")
+    #if any two sides are equal then the triangle is an isosceles triangle
     elif(a == b or b == c or a == c):
         print("The triangle is an isosceles triangle")
+    #if all sides are different then the triangle is a scalene triangle5
     else:
         print("The triangle is a scalene triangle")
 else:
