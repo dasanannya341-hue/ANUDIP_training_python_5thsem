7a207fff7bd2ab4b49285f70e592db6fb195d7d9
dasanannya341-hue <dasanannya341@gmail.com>
2026-06-03 16:00:01 +0530

    python

---
+++ SELF-LEARNING/typeoftringleonside.py
@@ -0,0 +1,24 @@
+#program to check the type of triangle based on its sides
+a = int(input("Enter the first side: "))
+if(a <= 0):
+    exit("Side cannot be negative or zero")
+#------------------------------------------------------
+b = int(input("Enter the second side: "))
+if(b <= 0):
+    exit("Side cannot be negative or zero")
+#------------------------------------------------------
+c = int(input("Enter the third side: "))
+if(c <= 0):
+    exit("Side cannot be negative or zero")
+#check whether the sum of any two sides is greater than the third side or not
+#if the sum of any two sides is greater than the third side then the triangle is valid otherwise it is not valid
+if(a + b > c and a + c > b and b + c > a):
+    print("The triangle is valid")
+    if(a == b == c):
+        print("The triangle is an equilateral triangle")
+    elif(a == b or b == c or a == c):
+        print("The triangle is an isosceles triangle")
+    else:
+        print("The triangle is a scalene triangle")
+else:
+    print("The triangle is not valid")
