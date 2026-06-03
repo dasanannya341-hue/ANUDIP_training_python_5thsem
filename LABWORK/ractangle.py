b06ebdcf81c937050e05715bc23764aab83335ca
dasanannya341-hue <dasanannya341@gmail.com>
2026-06-03 14:49:45 +0530

    python

---
+++ LABWORK/ractangle.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the length of the rectangle: "))
+b = int(input("Enter the breadth of the rectangle: "))
+area = a * b
+perimeter = 2 * (a + b)
+print("The area of the rectangle is:", area)
+print("The perimeter of the rectangle is:", perimeter)
