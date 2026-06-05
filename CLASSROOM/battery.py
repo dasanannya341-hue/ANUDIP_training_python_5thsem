677316d5f7c20583f7a10789e1ac9517dbb08305
dasanannya341-hue <dasanannya341@gmail.com>
2026-06-05 09:28:01 +0530

    python

---
+++ CLASSROOM/battery.py
@@ -0,0 +1,7 @@
+#program for displaying battery charging level
+charging_level = 20
+while(charging_level <= 100):
+		print("Battery Level : ",charging_level,"%")
+		charging_level = charging_level + 10
+#-----------------------------------------------
+print("Full Charge")
